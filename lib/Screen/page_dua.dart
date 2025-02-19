b9ea9b40b19a68c60b82ffa972f3c38e6e4dc20c
Chelsi Juliana Rizky <chelsijuliana10@gmail.com>
2025-02-19 22:20:08 +0700

    Initial commit

---
+++ lib/Screen/page_dua.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PageDua extends StatelessWidget {
+  const PageDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Page Dua'),
+          backgroundColor: Colors.brown,
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.home,
+                size: 35,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.spa,
+                size: 35,
+                color: Colors.purple,
+              ),
+              Icon(
+                Icons.phone,
+                size: 35,
+                color: Colors.red,
+              ),
+            ],
+          )
+        )
+    );
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobile/Screen/page_dua.dart';
+import 'package:flutter_mobile/Screen/page_empat.dart';
+import 'package:flutter_mobile/Screen/page_tiga.dart';
 
-void main() {
+
+import 'Screen/page_satu.dart';void main() {
   runApp(const MyApp());
 }
 
@@ -48,18 +52,90 @@ class PageOne extends StatelessWidget {
         //appBar : properti dari sebuah widget
         //AppBar : widget
 
-
-        title: Text('Aplikasi Pertama',
+        title: Text(
+          'Aplikasi Pertama',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.brown,
       ),
       body: Center(
-        child: Text("Selamat Datang di Flutter App Pertama MI2B"),
-        //child : cuma bisa nampung satu widget
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Selamat Datang di Flutter App pertama Chelsi Juliana Rizky"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(onPressed: (){
+                //ini untuk pindah ke page 1
+                Navigator.push(context, MaterialPageRoute(builder: (context)
+                => PageSatu()
+                ));
+              },
+                elevation: 18.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                //ini untuk pindah page
+                color: Colors.brown,
+                child: Text('Page 1',
+                  style: TextStyle(fontSize: 14, color: Colors.white ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(onPressed: (){
+                //ini untuk pindah ke page 1
+                Navigator.push(context, MaterialPageRoute(builder: (context)
+                => PageDua()
+                ));
+                },
+                elevation: 18.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                //ini untuk pindah page
+                color: Colors.brown,
+                child: Text('Page 2',
+                  style: TextStyle(fontSize: 14, color: Colors.white ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(onPressed: (){
+                //ini untuk pindah ke page 1
+                Navigator.push(context, MaterialPageRoute(builder: (context)
+                => PageTiga()
+                ));},
+                elevation: 18.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                //ini untuk pindah page
+                color: Colors.brown,
+                child: Text('Page 3',
+                  style: TextStyle(fontSize: 14, color: Colors.white ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(onPressed: (){
+                //ini untuk pindah ke page 1
+                Navigator.push(context, MaterialPageRoute(builder: (context)
+                => PageEmpat()
+                ));},
+                elevation: 18.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                //ini untuk pindah page
+                color: Colors.brown,
+                child: Text('Page 4',
+                  style: TextStyle(fontSize: 14, color: Colors.white ),
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        //child :  cuma bisa nampung 1 widget
         //children : bisa nampung beberapa widget
       ),
     );
   }
 }
-
